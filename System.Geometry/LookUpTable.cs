74c601922d9d495aa6bde8be593cdd9916f2edcd
lk <lk@git.com>
2019-10-31 10:22:04 +0100

    IReadOnlyList<Vector2> implemented

--- System.Geometry/LookUpTable.cs
+++ System.Geometry/LookUpTable.cs
@@ -6,13 +6,23 @@ using System.Text;
 
 namespace System.Geometry
 {
-    public class LookUpTable : IEnumerable<Vector2>
+    public class LookUpTable : IEnumerable<Vector2>, IReadOnlyList<Vector2>
     {
         private readonly int Steps;
         private readonly Bezier Bezier;
         private readonly double[] arcLengths;
         private readonly double length;
         private List<Vector2> _lut = new List<Vector2>();
+
+        public int Count
+        {
+            get { return Steps; }
+        }
+
+        public Vector2 this[int index]
+        {
+            get { return GetLUT()[index]; }
+        }
 
         /// <summary>
         /// Generates a LookUp Table of coordinates on the curve, spaced at parametrically equidistance intervals. If steps is given, the LUT will contain steps coordinates representing the coordinates from t=0 to t=1 at interval 1/steps.
@@ -105,7 +115,7 @@ namespace System.Geometry
 
 
         /// <summary>
-        /// Finds the on-curve point closest to the specific off-curve point, using a two-pass projection test based on the curve's LUT.
+        /// Finds the on-curve point closest to the specific off-curve point, using a three-pass projection test based on the curve's LUT.
         /// A distance comparison finds the closest match, after which a fine interval around that match is checked to see if a better projection can be found.
         /// </summary>
         public Vector2 Project(Vector2 point)
@@ -114,7 +124,7 @@ namespace System.Geometry
         }
 
         /// <summary>
-        /// Finds the on-curve point closest to the specific off-curve point, using a two-pass projection test based on the curve's LUT.
+        /// Finds the on-curve point closest to the specific off-curve point, using a three-pass projection test based on the curve's LUT.
         /// A distance comparison finds the closest match, after which a fine interval around that match is checked to see if a better projection can be found.
         /// </summary>
         public Vector2 Project(Vector2 point, out double t, out double d)
@@ -185,7 +195,7 @@ namespace System.Geometry
             Vector2 p = Bezier.Position(ft);
             t = ft;
             d = mdist;
-          
+
             return p;
 
         }
